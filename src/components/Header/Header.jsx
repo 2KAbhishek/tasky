033bec9b1885c937a57b6ad6b1aa343620397fe5
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-06 14:36:46 +0530

    Add button to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css';
+import Button from '../Button/Button';
 
 const Header = () => {
   return (
     <header className="Header">
       <h1>Tasky</h1>
+      <Button/>
     </header>
   );
 };
