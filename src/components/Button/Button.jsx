e71afa27efc91ed99872d4b40cd1837e1588a1eb
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-06 15:04:34 +0530

    Add defaultProps and PropType validatopns to Buttons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,22 @@
+import PropTypes from 'prop-types';
 import './Button.css';
 
-const Button = () => {
+const Button = ({color, text}) => {
   return (
-    <button className="Button">
-        Add
+    <button className="Button" style={{backgroundColor: color}}>
+      {text}
     </button>
   );
+};
+
+Button.defaultProps = {
+  color: 'royalblue',
+  text: 'Add',
+};
+
+Button.propTypes = {
+  color: PropTypes.string,
+  text: PropTypes.string,
 };
 
 export default Button;
