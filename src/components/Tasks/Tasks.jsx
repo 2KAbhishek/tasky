f6d572cbba1d3e5df3df171f9a23a78700af2bb1
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-08 00:36:22 +0530

    Pass onToggle

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import Task from '../Task/Task';
 
-const Tasks = ({tasks, onDelete}) => {
+const Tasks = ({tasks, onDelete, onToggle}) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete}/>
+        <Task key={task.id} task={task}
+          onDelete={onDelete} onToggle={onToggle}/>
       ))}
     </>
   );
@@ -14,6 +15,7 @@ const Tasks = ({tasks, onDelete}) => {
 Tasks.propTypes = {
   tasks: PropTypes.array,
   onDelete: PropTypes.func,
+  onToggle: PropTypes.func,
 };
 
 export default Tasks;
