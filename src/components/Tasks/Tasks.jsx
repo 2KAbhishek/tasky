4fd648189a4b2129ef3e548f1e31d67e09964db4
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-07 11:53:18 +0530

    Use Task component

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import Task from '../Task/Task';
 
 const Tasks = ({tasks}) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <h3 key={task.id}> <Task task={task}/></h3>
       ))}
     </>
   );
