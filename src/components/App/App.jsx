d9ff183ac2b92ccef87784017c6767b508cc37ea
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-07 10:46:03 +0530

    Global state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,32 @@
+import {useState} from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import Tasks from '../Tasks/Tasks';
+
 /**
  * Main app component
  * @return {*}
  */
 const App = () => {
+  const [tasks] = useState([
+    {
+      id: 1,
+      text: 'Build Projects',
+      day: 'June 13th',
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: 'Add to portfolio',
+      day: 'June 16th',
+      reminder: true,
+    },
+  ]);
+
   return (
     <div className="App">
       <Header title={'Tasky'}/>
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 };
