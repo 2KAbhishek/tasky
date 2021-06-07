4f3ed372b6a870b5e639a2cf8c7342660ea41f5b
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-07 13:35:18 +0530

    Add FaTimes icon

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 import './Task.css';
+import {FaTimes} from 'react-icons/fa';
 
 const Task = ( {task} ) => {
   return (
     <div className="Task">
-      <h3>{task.text}</h3>
+      <h3>{task.text}
+        <FaTimes style = {{color: 'red', cursor: 'pointer'}}/></h3>
+      <p>{task.day}</p>
     </div>
   );
 };
