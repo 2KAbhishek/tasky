20c4b26467cac3e32b4fb37c09e70dc4bd6c5d23
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-12 17:26:37 +0530

    Revert "Toggle tasks on single click"
    This reverts commit 4f6917e43d0820201b5da71999eec946ecb33c47.

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -5,7 +5,7 @@ import {FaTimes} from 'react-icons/fa';
 const Task = ( {task, onDelete, onToggle} ) => {
   return (
     <div className={`Task ${task.reminder ? 'reminder' : ''}`}
-      onClick={() => onToggle(task.id)}>
+      onDoubleClick={() => onToggle(task.id)}>
       <h3>{task.text}
         <FaTimes onClick={() => onDelete(task.id)}
           style = {{color: 'red', cursor: 'pointer'}}/></h3>
