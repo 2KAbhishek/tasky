55254485097c9db592fdae8fd4b7fbcb68a0fc59
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-13 05:12:11 +0530

    Use trash icon

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 import './Task.css';
-import {FaTimes} from 'react-icons/fa';
+import {FaTrash} from 'react-icons/fa';
 
 const Task = ( {task, onDelete, onToggle} ) => {
   return (
     <div className={`Task ${task.reminder ? 'reminder' : ''}`}
       onDoubleClick={() => onToggle(task.id)}>
       <h3>{task.text}
-        <FaTimes onClick={() => onDelete(task.id)}
+        <FaTrash onClick={() => onDelete(task.id)}
           style = {{color: 'red', cursor: 'pointer'}}/></h3>
       <p>{task.day}</p>
     </div>
