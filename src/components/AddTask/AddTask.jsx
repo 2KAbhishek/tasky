c195d836f492a21cab26bf3a94c7ec583c56f6de
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-15 05:16:42 +0530

    Remove reminder checkbox

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -10,11 +10,6 @@ const AddTask = () => {
         <label htmlFor="Date"></label>
         <input type="datetime-local" name="Date" id="date" />
       </div>
-      <div className="form-checkbox">
-        <label htmlFor="Reminder"></label>
-        <input type="checkbox" name="" id="reminder" />
-      </div>
-
       <Button color={'#1688f0'} text={'Save'} type={'submit'}
         width= {'98%'}/>
     </form>
