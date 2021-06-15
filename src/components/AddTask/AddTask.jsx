56f583c48af0a15deea225699e0ca0063c5a5bc6
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-06-15 06:13:22 +0530

    Add local state for Task elements

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,15 +1,21 @@
+import {useState} from 'react';
 import './AddTask.css';
 import Button from '../Button/Button';
+
 const AddTask = () => {
+  const [text, setText] = useState('');
+  const [time, setTime] = useState('');
   return (
     <form>
       <div className="form-input">
         <label htmlFor="Task"></label>
-        <input type="text" name="Task" placeholder="Add Task" id="task" />
+        <input type="text" name="Task" placeholder="Add Task"
+          id="task" value={text} onChange={(e) => setText(e.target.value)}/>
       </div>
       <div className="form-text">
         <label htmlFor="Date"></label>
-        <input type="datetime-local" name="Date" id="date" />
+        <input type="datetime-local" name="Date"
+          id="date" value={time} onChange={(e) => setTime(e.target.value)} />
       </div>
       <Button text={'Save'} type={'submit'}
         width= {'98%'}/>
